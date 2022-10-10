3e0acc1663fabc221222867dafb4e50890b9d5b2
ATEIS\a22gonzalogg <gonzalo_gg5@hotmail.com>
2022-10-10 17:03:31 +0200

    Cambio 10/10/2022

---
+++ EjemploC.c
@@ -0,0 +1,23 @@
+
+#include <studio.h>
+#include <conio.h>
+
+int funcionSuma(int a, int b) 
+	
+
+void main(){
+	int n1, n2, suma;
+	
+	
+	printf ("\n Introduzca primer número: \n");
+	scanf("%d", &nl);
+	printf ("\n Introduzca segundo número : \n");
+	scanf("%d",&n2);
+	
+
+	suma= funcionSuma(n1,n2);
+printf ("\n La suma es: %d", suma);
+}
+	int funcionSuma(int a, int b) {
+	return a+b;
+}
